36fed7935f76559151dfbd161a1f315aca39328a
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-04-22 13:07:42 -0500

    hoy

---
+++ tsp/Rainbow/Rainbow/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by macbook on 4/22/19.
+//  Copyright © 2019 nidem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "5"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
